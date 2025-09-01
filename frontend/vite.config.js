6d10b304e1860608ba05ad05d17013c6ddbd2bcd
DpkRn <deepak@skor.co>
2025-09-01 21:00:21 +0530

    1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",   // 👈 important
   build: {
     outDir: "dist"
-  },
-  server: {
-    historyApiFallback: true
   }
-});
+})
