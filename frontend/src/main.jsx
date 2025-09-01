c017ffe54fd2537868992974f0704d7af762a61d
DpkRn <deepak@skor.co>
2025-09-01 20:03:32 +0530

    fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/map" element={<MapPage />} />
+        {/* <Route path="/map" element={<MapPage />} /> */}
       </Routes>
     </BrowserRouter>
   </StrictMode>,
